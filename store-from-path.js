43df1d7026c34439f1cfc89167d571497e797ed6
m <13090422+marciob@users.noreply.github.com>
2022-09-27 11:52:05 -0300

    first commit

--- store-from-path.js
+++ store-from-path.js
@@ -4,6 +4,7 @@ import { Web3Storage, getFilesFromPath } from "web3.storage";
 import dotenv from "dotenv";
 dotenv.config();
 
+//token access - got from https://web3.storage
 const WEB3_STORAGE_API = process.env.WEB3_STORAGE_API;
 
 async function main() {
@@ -16,18 +17,22 @@ async function main() {
     );
   }
 
+  //the script requires passing the file/directory path when running
   if (args._.length < 1) {
     return console.error("Please supply the path to a file or directory");
   }
 
+  //instante the web3storage
   const storage = new Web3Storage({ token });
   const files = [];
 
+  //each file/directory passed as argument is placed in files array
   for (const path of args._) {
     const pathFiles = await getFilesFromPath(path);
     files.push(...pathFiles);
   }
 
+  //it stores data from files array to IPFS and returns the cid
   console.log(`Uploading ${files.length} files`);
   const cid = await storage.put(files);
   console.log("Content added with CID:", cid);
